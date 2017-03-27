cb0ef3226a1023c3ceeb25aa0520db272bebb713
MLFontaine <marcusfontaine@gmail.com>
2017-03-26 20:27:07 -0400

    Python update
    Got database table loaded into pandas dataframe

--- mpg_project/mpg_analysis2.py
+++ mpg_project/mpg_analysis2.py
@@ -1,25 +1,36 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
+#mpg_analysis commands
 
 import MySQLdb as mdb
 import sys
+import pandas as pd
+import pandas.io.sql
 
-try:
-    con = mdb.connect(host='localhost', db='mpg_db', read_default_file='~/.my.cnf');
-    
-    cur = con.cursor()
-    cur.execute("SELECT VERSION()")
-    
-    ver = cur.fetchone()
-    
-    print "Database version : %s " % ver
+#import mysql.connector as sql
+#db_con = sql.connect(host='localhost', database='mpg_db', option_files=os.path.expanduser('~/.my.cnf')) #couldnt get this to read my password, only got username for some reason
 
-except mdb.Error, e:
-    
-    print "Error %d: %s" % (e.args[0],e.args[1])
-    sys.exit(1)
+con = mdb.connect(host='localhost', db='mpg_db', read_default_file='~/.my.cnf')
 
-finally:
-    
-    if con:
-        con.close()
+
+
+gas_data = pandas.read_sql("SELECT * FROM mpg_table", con)
+
+print(type(gas_data)) #shows type as pandas dataframe
+
+
+
+
+
+#con = mdb.connect(host='localhost', db='mpg_db', read_default_file='~/.my.cnf');
+#    
+#with con:
+#    
+#    cur = con.cursor(mdb.cursors.DictCursor)
+#    cur.execute("SELECT * FROM mpg_table LIMIT 4")
+#    
+#    rows = cur.fetchall()
+#    
+#    for row in rows:
+#        print row["thedate"], row["mpg"]
+#
+#if con:
+#    con.close()

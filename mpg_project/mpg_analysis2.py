eca41b24412e78787f387708fdffde1ccf232752
MLFontaine <marcusfontaine@gmail.com>
2017-05-08 19:24:36 -0400

    new folder for testing bokeh

--- mpg_project/mpg_analysis2.py
+++ mpg_project/mpg_analysis2.py
@@ -73,14 +73,6 @@ print(log_lm.summary())
 #plt.show()
 
 
-#test bokeh
-from bokeh.plotting import figure, output_file, show
-
-p = figure(width=400, height=400)
-p.circle(2, 3, radius=.5, alpha=0.5, color='red')
-output_file('out.html')
-show(p)
-
 print('end')
 
 
